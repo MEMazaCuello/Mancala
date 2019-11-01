0a0ab9cce81850e9b41e22e287b6665d6f13bafe
MEMazaCuello <martin.mazacuello@gmail.com>
2019-11-01 17:48:29 +0100

    Minor correction

--- frontCell.js
+++ frontCell.js
@@ -1,9 +1,9 @@
 class FrontCell extends Cell {
   // FrontCell()
   constructor(x, y, r, c, idx) {
-    // FrontCell is just a Cell that is
+    // FrontCell is just a Cell that
     // knows which Cell it has in front
-    // (the enemy Cell) and which is back 
+    // (the enemy Cell) and which is back
     super(x, y, r, c, idx);
     this.enemy = 23 - idx;
     this.back  = 15 - idx;
